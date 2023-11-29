a72ea19b47924930304f7597cb2326ef1150fc73
aliouisall <aliouibnibrahim@gmail.com>
2023-11-29 14:44:48 +0100

    Rename tables and add entities to admin panel

--- LockLite/app/models.py
+++ LockLite/app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     user_email = models.CharField(max_length=50)
     user_password = models.CharField(max_length=50)
 
-class Credentials(models.Model):
+class Credential(models.Model):
     credentials_email = models.CharField(max_length=50)
     credentials_password = models.CharField(max_length=50)
     label = models.CharField(max_length=50)
