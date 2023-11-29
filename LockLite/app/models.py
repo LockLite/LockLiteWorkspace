1bfd29bffe0480f9ed679b23fa89c738158ff301
aliouisall <aliouibnibrahim@gmail.com>
2023-11-29 14:35:42 +0100

    Create model

--- LockLite/app/models.py
+++ LockLite/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_email = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=50)
+
+class Credentials(models.Model):
+    credentials_email = models.CharField(max_length=50)
+    credentials_password = models.CharField(max_length=50)
+    label = models.CharField(max_length=50)
