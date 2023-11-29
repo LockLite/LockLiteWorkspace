b9a12aaea52f7aeb5c8dab1d4e0ba616b17d664c
vbetsch <vic.betsch@gmail.com>
2023-11-29 16:05:36 +0100

    :sparkles: PostgreSQL configuration

--- LockLite/app/models.py
+++ LockLite/app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     user_email = models.CharField(max_length=50)
     user_password = models.CharField(max_length=50)
+
 
 class Credential(models.Model):
     credentials_email = models.CharField(max_length=50)
