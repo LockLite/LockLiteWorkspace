de17910a8ffda18cc81b10f5af79c22e88019c76
aliouisall <aliouibnibrahim@gmail.com>
2023-11-29 16:35:13 +0100

    :truck: Move app to api folder

--- LockLite/api/models.py
+++ LockLite/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_email = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=50)
+
+
+class Credential(models.Model):
+    credentials_email = models.CharField(max_length=50)
+    credentials_password = models.CharField(max_length=50)
+    label = models.CharField(max_length=50)
