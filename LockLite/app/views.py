38dd14325099a1c65ec2eeb4f7559ce4778f90df
vbetsch <vic.betsch@gmail.com>
2024-01-10 15:15:35 +0100

    :sparkles: First view

--- LockLite/app/views.py
+++ LockLite/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import User
+
+
 # Create your views here.
+def index(request, *args, **kwargs):
+	test = User.objects.all()
+	data = {
+		'name': "Test",
+		'users': test
+	}
+	return render(request, 'index.html', data)
