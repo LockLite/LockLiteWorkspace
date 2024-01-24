07637680515e93b986de9adf2d56fa800309b3a0
vbetsch <vic.betsch@gmail.com>
2024-01-24 12:17:13 +0100

    :wrench: Static files configuration

--- LockLite/LockLite/urls.py
+++ LockLite/LockLite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('', views.index, name='index'),
@@ -24,3 +26,6 @@ urlpatterns = [
 	path('test', views.test, name='test'),
 	path('signup', views.sign_up, name='signup')
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
