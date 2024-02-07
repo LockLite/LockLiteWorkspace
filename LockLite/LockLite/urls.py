10c8c59180b2fceafff9c951b05f6eaa109abba9
vbetsch <vic.betsch@gmail.com>
2024-02-07 10:32:14 +0100

    :recycle: Clean code

--- LockLite/LockLite/urls.py
+++ LockLite/LockLite/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
 	path('register', views.register, name='register'),
 	path("login/", auth_views.LoginView.as_view(), name="login"),
 	path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-	path('app/login/', auth_views.LoginView.as_view(template_name='app/login.html'))
 ]
 
 if settings.DEBUG:
