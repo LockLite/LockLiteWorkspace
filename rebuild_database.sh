7daf6507627562999d2e6081a960be3dc2928f4a
vbetsch <vic.betsch@gmail.com>
2024-02-07 15:17:16 +0100

    :hammer: Drop app_credential table if exists

--- rebuild_database.sh
+++ rebuild_database.sh
@@ -1,5 +1,6 @@
+docker exec locklite-postgresql psql -U locklite -d locklite -c "DROP TABLE IF EXISTS app_credential;"
 rm -rf LockLite/app/migrations
-docker exec locklite-postgresql psql -U locklite -d locklite -c "DELETE FROM django_migrations WHERE app = 'app'"
+docker exec locklite-postgresql psql -U locklite -d locklite -c "DELETE FROM django_migrations WHERE app = 'app';"
 python LockLite/manage.py makemigrations app
 python LockLite/manage.py migrate
 python LockLite/manage.py loaddata initial_data.json
