67d6c2b82814f4992d2f686d56ad5549983fba3d
vbetsch <vic.betsch@gmail.com>
2024-04-24 16:34:46 +0200

    :sparkles: _test_url

--- LockLite/app/tests/test_urls.py
+++ LockLite/app/tests/test_urls.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 
 class TestsUrls(TestCase):
+	def _test_url(self, route, kwargs=None):
+		path = reverse(route)
+		match = resolve(path, kwargs)
+		self.assertEqual(match.view_name, route)
+
 	def setUp(self):
 		pass
 
 	def test_login(self):
-		hello = 'hello'
-		self.assertEqual(hello, 'hello')
+		self._test_url('login')
+
+	def test_register(self):
+		self._test_url('register')
